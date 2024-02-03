2e388908c5d40af7b54c47764a2abe6846fdbb75
Mackay89 <manotomackay@gmail.com>
2024-02-02 17:30:15 -0800

    text

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -4,6 +4,47 @@
  * hash_table_set - Adds an element to the hash table
  * @ht: A pointer to the hash table.
  * @key: The key to add - cannot be an empty string.
- * @value: The value associated with key.
+ * @value: The value to add the element.
  * Return: UPON FAILURE - 0, Otherwise - 1.
  */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index = 0;
+	char *value_copy, *key_copy;
+	hash_node_t *bucket, *new_node;
+
+	if (!ht || !key || !value)
+		return (0);
+
+	value_copy = strdup(value);
+	if (!value_copy)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	bucket = ht->array[index];
+
+	while (bucket)
+	{
+		if (!strcmp(key, bucket->key))
+		{
+			free(bucket->value);
+			bucket->value = value_copy;
+			return (1);
+		}
+		bucket = bucket->next;
+	}
+	new_node = calloc(1, sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	key_copy = strdup(key);
+	if (!key_copy)
+		return (0);
+	new_node->key = key_copy;
+	new_node->value = value_copy;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
