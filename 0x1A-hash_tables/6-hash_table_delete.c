afb53a1e84b59be21e934c21f5873c7fa57772c2
Mackay89 <manotomackay@gmail.com>
2024-02-02 18:32:17 -0800

    text

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -4,27 +4,28 @@
  * hash_table_delete - Deletes a hash table.
  * @ht: The hash table to be deleted.
  */
-void hash_table_delete(hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht)
 {
-	hash_table_t *head = ht;
-	hash_node_t *n, *tmp;
+	hash_node_t *node, *tmp;
 	unsigned long int i;
+
+	if (!ht)
+		return;
 
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i] != NULL)
+		node = ht->array[i];
+		while (node)
 		{
-			n = ht->array[i];
-			while (n != NULL)
-			{
-				tmp = n->next;
-				free(n->key);
-				free(n->value);
-				free(n);
-				n = tmp;
-			}
+			tmp = node;
+			node = node->next;
+			if (tmp->key)
+				free(tmp->key);
+			if (tmp->value)
+				free(tmp->value);
+			free(tmp);
 		}
 	}
-	free(head->array);
-	free(head);
+	free(ht->array);
+	free(ht);
 }
